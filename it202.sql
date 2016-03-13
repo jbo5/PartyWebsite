ad84ceeb083b19638d8d47aa8d7719bd9f16df15
jbo5 <jbo5@njit.edu>
2016-03-12 23:25:59 -0500

    two tables created

---
+++ it202.sql
@@ -0,0 +1,24 @@
+drop table if exists clients;
+create table clients
+  (
+    clientId INT(11) primary key auto_increment,
+    clientName varchar(32),
+    clientPW varchar(64),
+    activeSession varchar(128),
+    firstLogin datetime,
+    lastLogin datetime
+    
+  );
+  
+ drop table if exists parties;
+ create table parties
+  (
+    partyId INT(11) primary key auto_increment,
+    clientName varchar(32),
+    partyName varchar(32),    
+    partyLocation varchar(32),
+    partyTime dateTime,
+    partyComments varchar(32)
+    
+  
+  );
