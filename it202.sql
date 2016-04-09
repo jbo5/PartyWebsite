aa679897aa2e485e619a995c613b112e24fa9326
jbo5 <jbo5@njit.edu>
2016-04-08 20:23:36 -0400

    Improved sql file

--- it202.sql
+++ it202.sql
@@ -1,5 +1,5 @@
-drop table if exists clients;
-create table clients
+ drop table if exists clients;
+ create table clients
   (
     clientId INT(11) primary key auto_increment,
     clientName varchar(32),
